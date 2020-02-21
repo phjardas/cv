edcd47e3721d5d28e2c3c8ae2ca911134e648943
Philipp Jardas <philipp@jardas.de>
2020-02-21 13:04:55 +0100

    Improve a11y: select contrast text color for heading

--- src/animator.js
+++ src/animator.js
@@ -1,24 +1,46 @@
+import { getContrastText } from './colors'
+
 const saturation = {
-  primary: '58%',
-  secondary: '100%',
+  primary: 58,
+  secondary: 100,
 }
 
 const brightness = {
-  primary: '42%',
-  primaryLight: '50%',
-  secondary: '65%',
+  primary: 42,
+  primaryLight: 50,
+  secondary: 65,
+}
+
+const text = {
+  dark: [33, 33, 33],
+  light: [255, 255, 255],
+  subtitle: [204, 204, 204],
 }
 
 function createStyles(hue) {
+  const primary = [hue, saturation.primary, brightness.primary]
+  const primaryLight = [hue, saturation.primary, brightness.primaryLight]
+  const primaryText = getContrastText(primary, text.light, text.dark)
+  const subtitleText = getContrastText(primary, text.subtitle, text.dark)
+  const secondary = [hue, saturation.secondary, brightness.secondary]
+  const secondaryText = getContrastText(secondary, text.light, text.dark)
+
   return {
-    '--primary': `hsl(${hue}, ${saturation.primary}, ${brightness.primary})`,
-    '--primary-light': `hsl(${hue}, ${saturation.primary}, ${
-      brightness.primaryLight
-    })`,
-    '--secondary': `hsl(${hue}, ${saturation.secondary}, ${
-      brightness.secondary
-    })`,
+    '--primary': `${hsl(primary)}`,
+    '--primary-text': `${rgb(primaryText)}`,
+    '--primary-light': `${hsl(primaryLight)}`,
+    '--subtitle-text': `${rgb(subtitleText)}`,
+    '--secondary': `${hsl(secondary)}`,
+    '--secondary-text': `${rgb(secondaryText)}`,
   }
+}
+
+function hsl(color) {
+  return `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`
+}
+
+function rgb(color) {
+  return `rgb(${color.join(', ')})`
 }
 
 function animateHue() {
