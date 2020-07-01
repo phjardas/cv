89d27a78fc1360b80549f4845de063f07fbc81a1
Philipp Jardas <philipp@jardas.de>
2020-07-01 17:03:03 +0200

    Add avatar

--- src/qr.js
+++
@@ -1,4 +0,0 @@
-import qr from './qr.svg'
-
-const aside = document.getElementsByClassName('aside')[0]
-aside.innerHTML = qr.replace('<svg', '<svg class="qr"')
--- src/script.js
+++ src/script.js
@@ -1,2 +1,1 @@
 import './animator'
-import './qr'
